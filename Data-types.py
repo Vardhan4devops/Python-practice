88390fe909d0a64014852a9494ba910b35490933
kbvardhan4devops <kbvardhan4devops@gmail.com>
2024-01-04 18:38:24 +0530

    updating python

---
+++ Data-types.py
@@ -0,0 +1,24 @@
+# string - string variables can be assigned in below ways
+str1 = 'alpha'
+str2 = "beta"
+str3 = '''gamma'''
+str4 = """delta"""
+
+# number
+int1 = 123
+flt1 = 2.0
+
+# list - collection of multiple data types - mutable(can be edited)
+list1 = [str1, "DevOps",int1, 2.0]
+
+print(list1)
+
+# touple - collection of multiple data types - immutable(can not be edited)
+touple1 = (str1, "DevOps", int1, 2.0)
+
+print (touple1)
+
+# Dictionary -  collection of Key & value
+dictionary1 = {"Name":"Vardhan", "Weight":75, "Exercises":["Boxing", "Dancing", "Jogging"]}
+
+print(dictionary1)
