4ed9a5456545988093e5ca5b9472a04824df777f
kbvardhan4devops <kbvardhan4devops@gmail.com>
2024-01-04 17:53:27 +0530

    python practice-1

---
+++ Variables.py
@@ -0,0 +1,26 @@
+#variable assignment
+var1 = "Python"
+var2=123
+var3 =3.5
+
+print(var1)
+print(var2)
+print(var3)
+
+a=b=c=100
+print(a)
+print(b)
+print(c)
+
+# multi assignment
+x,y,z="alpha","beta","gamma"
+
+print(x)
+print(y)
+print(z)
+
+# to find type of variable:
+print(type(a))
+print(type(b))
+print(type(x))
+print(type(var3))
---
+++ python-syntax.py
@@ -0,0 +1,13 @@
+x=2
+print("learning indentation")
+print()
+
+if x==0:
+    print("in the if block")
+    print("x=0")
+else:
+    print("in the else block")
+    print("x is non zero")
+
+print()
+print("out of if/else block")
