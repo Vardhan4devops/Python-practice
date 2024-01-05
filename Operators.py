18e4bf8d628b9cc1e3d3bd12c3ba4a350472351c
kbvardhan4devops <kbvardhan4devops@gmail.com>
2024-01-05 19:45:29 +0530

    updating operators

---
+++ Operators.py
@@ -0,0 +1,101 @@
+# Arithmetic operators
+x=2
+y=5
+
+total=x+y
+print(total)
+
+total=x-y
+print(total)
+
+total=x*y
+print(total)
+
+total=y/x
+print(total)
+
+total=y%x
+print(total)
+
+total=y**x
+print(total)
+
+# Comparison operators
+a=30;
+b=60;
+
+out=(a<b)
+print(out)
+
+out=(a>b)
+print(out)
+
+out=(a==b)
+print(out)
+
+out=(a!=b)
+print(out)
+
+out=(a>=b)
+print(out)
+
+out=(a<=b)
+print(out)
+
+# Assignment operators
+c=0;
+d=1;
+
+c+=d # c=c+d
+print(c)
+
+c-=d # c=c-d
+print(c)
+
+# Logical operators (or/and)
+a=30;
+b=60;
+
+x=2;
+y=3;
+
+result=(a>b) or (x<y)
+print(result)
+
+result=(a>b) or (x>y)
+print(result)
+
+result=(a>b) and (x>y)
+print(result)
+
+result=(a<b) and (x<y)
+print(result)
+
+result=x<y
+print(result)
+
+result=not(x<y)
+print(result)
+
+# Membership operator
+list1=["DevOps",123,"IOT"]
+ans="DevOps" in list1
+print(ans)
+
+ans="AWS" in list1
+print(ans)
+
+ans="AWS" not in list1
+print(ans)
+
+# Identity operators
+a=10;
+b=20;
+
+output=a is b
+print(output)
+
+output=a is not b
+print(output)
+
+
