a74e9f165d456eac0f676150d169f201664d84e3
kbvardhan4devops <kbvardhan4devops@gmail.com>
2024-01-08 21:42:24 +0530

    updating conditions

---
+++ Conditions.py
@@ -0,0 +1,35 @@
+# IF
+x=21
+
+if (x<30):
+    print("Inside IF block")
+    print("X value is: ",x)
+
+print("Rest of the code")
+
+# IF/ELSE
+x=11
+
+if x<10:
+    print("x is less than 11")
+    print("within the if block")
+
+else:
+    print("x is greater than 10")
+    print("in else block")
+
+# IF/Elif/else
+
+x=100;
+
+if x>100:
+    print("x is greater than 100")
+
+elif x==100:
+    print("x is equal to 100")
+
+else:
+    print("x is less than 100")
+
+
+
