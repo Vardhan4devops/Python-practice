f9195ffa1612c3f99b26f837f681ff27a13ecc19
kbvardhan4devops <kbvardhan4devops@gmail.com>
2024-01-13 21:38:15 +0530

    updatigng loops

---
+++ Loops.py
@@ -0,0 +1,15 @@
+# For loop
+DevOps = ("Agile","Cloud","CI/CD", "Scripting", "Automation","VCS")
+for i in DevOps:
+    print(i)
+
+print("Rest of the code")
+
+# while loop
+x=0;
+
+while x<=10:
+    print(x)
+    x=x+1
+
+print("Rest of the code")
