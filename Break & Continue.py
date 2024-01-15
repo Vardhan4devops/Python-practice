dcb787bacae61ae9ead98b1417a3433b25edfbf6
kbvardhan4devops <kbvardhan4devops@gmail.com>
2024-01-15 20:42:30 +0530

    updatigng break

--- Break & Continue.py
+++ Break & Continue.py
@@ -1,3 +1,4 @@
+'''
 # Break
 vaccines = ["covaxin","covishield","Sputnik V","Pfizer","AstraZeneca"]
 for i in vaccines:
@@ -14,3 +15,47 @@ for i in vaccines:
         continue
     print(f"Found vaccine: {i}")
 print("Out of the loop")
+'''
+'''
+# Random/shuffle - break
+import random
+vaccines = ["covaxin","covishield","Sputnik V","Pfizer","AstraZeneca"]
+
+random.shuffle(vaccines)
+print(vaccines)
+
+my_vaccine=random.choice(vaccines)
+print("My vaccine is: ",my_vaccine)
+
+for vac in vaccines:
+    print(f"********Testing {vac}")
+    if vac==my_vaccine:
+        print("******************************")
+        print(f"Test is successful for {vac}")
+        print()
+        break
+    print("******************************")
+    print("Test failed")
+    print()
+'''
+# Random/shuffle - continue
+import random
+vaccines = ["covaxin", "covishield", "Sputnik V", "Pfizer", "AstraZeneca"]
+
+random.shuffle(vaccines)
+print(vaccines)
+
+my_vaccine = random.choice(vaccines)
+print("My vaccine is: ", my_vaccine)
+
+for vac in vaccines:
+    print(f"********Testing {vac}")
+    if vac == my_vaccine:
+        print("******************************")
+        print(f"Test is successful for {vac}")
+        print()
+        continue
+    print("******************************")
+    print("Test failed")
+    print()
+
