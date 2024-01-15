02a70fda3a9fc96a506fb610a89a4b0dcd4c4065
kbvardhan4devops <kbvardhan4devops@gmail.com>
2024-01-15 20:07:18 +0530

    updatigng break

---
+++ Break & Continue.py
@@ -0,0 +1,16 @@
+# Break
+'''vaccines = ["covaxin","covishield","Sputnik V","Pfizer","AstraZeneca"]
+for i in vaccines:
+    print(i)
+    if i == "Pfizer":
+        print("Found vaccine")
+        break
+print("Out of the loop")
+'''
+# Continue
+vaccines = ["covaxin","covishield","Sputnik V","Pfizer","AstraZeneca"]
+for i in vaccines:
+    if i == "Pfizer":
+        continue
+    print(f"Found vaccine: {i}")
+print("Out of the loop")
